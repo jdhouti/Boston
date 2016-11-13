2d7b71524d8de34edec894b69dcc5f5ba9c31cb4
Julien Dhouti <jd2wm@virginia.edu>
2016-11-12 23:10:24 -0500

    removed file_exists function
    + did this so I didn’t have to test individually in the formatting
    script.

--- filefunc.py
+++ filefunc.py
@@ -5,20 +5,16 @@
 
 from os.path import exists
 
-def create_file():                                   #Creates the file.
-    target = open("DATA.txt", "w")
+def create_file(name):
+    filename = name + "DATA.txt"                  #Creates the file.
+    target = open(filename, "w")
     target.write("# Please do not edit anything this file!\n")
     target.write("# height weight skm pbf")
 
-def write_file(a, b, c, d):
-    if exists("DATA.txt"):
-        with open("DATA.txt", "a") as myfile:         #temporarily renames file to myfile
+def write_file(a, b, c, d, name):
+    filename = name + "DATA.txt"
+
+    if exists(filename):
+        with open(filename, "a") as myfile:         #temporarily renames file to myfile
             string = "\n%s %s %s %s" % (a, b, c, d)
             myfile.write(string)                      #adds the given numbers to the file
-
-def file_exists():
-    if exists("DATA.txt"):                            #Checks if the file exists
-        return True
-
-    else:
-        return False
