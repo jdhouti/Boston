7fc6370f494f264cedd92a81cc8bf6fc076a3488
Julien Dhouti <julien@email.virginia.edu>
2016-11-23 17:47:50 -0700

    Testing.

--- graph_functions.py
+++ graph_functions.py
@@ -3,4 +3,18 @@
 # Started 11/11/2016
 # This script builds the graph.
 
-def buildGraph()
+import matplotlib.pyplot as plt
+
+def get_pbf_data():
+    jesseData = get_pbf_list("Jesse")
+    colinData = get_pbf_list("Colin")
+    charlieData = get_pbf_list("Charlie")
+    brianData = get_pbf_list("Brian")
+
+    return jesseData, colinData, charlieData, brianData
+
+def get_date():
+
+
+def graph():
+    x1, x2, x3, x4 = get_pbf_data()
